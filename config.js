13204fa4d2a502e83df12784fcdb9d52ee7f2483
Rockyannan007 <96523984+Rockyannan007@users.noreply.github.com>
2021-12-23 08:55:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {
- TOKEN_BOT: "HERE-TOKEN",
- PREFIX_BOT: "z!",
+ TOKEN_BOT: "ODU4MzY3NjM3MDUwNjIxOTcy.YNdHKA.aAaQntYyNhvgectcwp4_fUWSPcY",
+ PREFIX_BOT: "*",
  UNLIMITED_BANS: 3,
  UNLIMITED_KICKS: 10
 
